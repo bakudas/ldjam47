19bfe4390ef9b89fdc940c139c829a652f0e5a85
bakudas <bakudas@gmail.com>
2020-10-03 02:52:20 -0300

    polishing the camera system

---
+++ Assets/src/Scripts/CamTriggerRoom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTriggerRoom : MonoBehaviour
+{
+
+    public string cameraToSwitch;
+    private bool estaIndo;
+    private Animator camAnimator;
+
+    private void Start()
+    {
+        camAnimator = GameObject.FindGameObjectWithTag("camAnimator").GetComponent<Animator>();
+        estaIndo = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (estaIndo)
+        {
+            estaIndo = false;
+            camAnimator.SetBool(cameraToSwitch, estaIndo);
+        }
+        else
+        {
+            estaIndo = true;
+            camAnimator.SetBool(cameraToSwitch, estaIndo);
+        }
+        
+    }
+}
--- Assets/src/Scripts/cameraPOV.cs
+++ Assets/src/Scripts/cameraPOV.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 
 public class cameraPOV : MonoBehaviour
 {
+
+    public Animator camAnimator;
+
+    private void Start()
+    {
+        camAnimator = GameObject.FindGameObjectWithTag("camAnimator").GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        camAnimator.SetBool("camera3", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        camAnimator.SetBool("camera3", false);
     }
 }
