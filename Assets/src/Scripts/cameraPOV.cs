3fc0530ff86daf55b6db197691d68a4337b66b47
bakudas <bakudas@gmail.com>
2020-10-03 02:07:13 -0300

    some decorations and initial camera system

---
+++ Assets/src/Scripts/cameraPOV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraPOV : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
